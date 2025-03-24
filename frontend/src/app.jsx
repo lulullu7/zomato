3b0b9be30542e2bd91a0150df9395b26e7875a10
lulullu7 <lulu.fathima.km@gmail.com>
2025-03-24 12:54:17 +0530

    second and third section zomato design completed

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './App.scss'
+import { IoIosSearch } from "react-icons/io";
+import { FaLocationDot } from "react-icons/fa6";
+import { TiArrowSortedDown } from "react-icons/ti";
+import { MdArrowRight } from "react-icons/md";
 
 function app() {
   return (
     <div>
+      {/* section 1 */}
       <section className='first_section_main'>
         <div className='first_section_innerdiv'>
           <div className='first_section_nav'>
@@ -27,9 +32,12 @@ function app() {
               <select className='first_section_middle_location'>
                 <option> location</option>
               </select>
+              <FaLocationDot className='first_section_middle_location_icon' />
+              <TiArrowSortedDown className='first_section_middle_downarrow_icon' />
               <p className='line'>|</p>
 
-              <input type="text" placeholder='search for restaurant , cuisine or a dish' className='first_section_middle_search'/>
+              <input type="text" placeholder='search for restaurant , cuisine or a dish' className='first_section_middle_search' />
+              <IoIosSearch className='first_section_middle_search_icon' />
             </div>
 
           </div>
@@ -37,6 +45,68 @@ function app() {
         </div>
 
       </section>
+
+      {/* section 2 */}
+      <section className='second_section_main'>
+        <div className='second_section_innerdiv_main'>
+          <div className='second_section_card_main'>
+            <img className='second_section_card_image' height={'170px'} width={'100%'} src="https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp" alt="" />
+            <div className='second_section_card_content'>
+              <p style={{ fontSize: '23px', color: 'rgba(11, 11, 11, 0.83)' }}>Order online</p>
+              <p style={{ marginTop: '5px' }}>stay home and order to your dooorstep</p>
+            </div>
+          </div>
+          <div className='second_section_card_main'>
+            <img className='second_section_card_image' height={'170px'} width={'100%'} src="https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp" alt="" />
+            <div className='second_section_card_content'>
+              <p style={{ fontSize: '23px', color: 'black' }}>Order online</p>
+              <p style={{ marginTop: '5px' }}>stay home and order to your dooorstep</p>
+            </div>
+          </div>
+
+        </div>
+
+      </section>
+
+      {/* collection section 3 */}
+
+      <section className='third_section_main'>
+        <div className='third_section_innerdiv_main'>
+          <div className='third_section_heading'>Collection</div>
+          <div className='third_section_subheading_main'>
+            <div className='third_section_subheading_left'>
+              <p>Explore curated lists of top restaurants, cafes, pubs, and bars in Kochi, based on trends</p>
+            </div>
+            <div className='third_section_subheading_right'>
+              <p>All collections in Kochi</p>
+              <MdArrowRight style={{ fontSize: '30px' }} />
+            </div>
+          </div>
+          <div className='third_section_card_main'>
+            <div className='third_section_card_onebyone card_one' >
+              <p className='third_section_card_onebyone_heading'>Places for art lovers</p>
+              <p className='third_section_card_onebyone_subheading'>7 places       <MdArrowRight style={{ fontSize: '30px' }} /></p>
+
+            </div>
+            <div className='third_section_card_onebyone card_two'>
+              <p className='third_section_card_onebyone_heading'>Places for art lovers </p>
+              <p className='third_section_card_onebyone_subheading'>7 places <MdArrowRight style={{ fontSize: '30px' }} /></p>
+            </div>
+            <div className='third_section_card_onebyone card_three'>
+              <p className='third_section_card_onebyone_heading'>Places for art lovers</p>
+              <p className='third_section_card_onebyone_subheading'>7 places <MdArrowRight style={{ fontSize: '30px' }} /></p>
+            </div>
+            <div className='third_section_card_onebyone card_four'>
+              <p className='third_section_card_onebyone_heading'>Places for art lovers </p>
+              <p className='third_section_card_onebyone_subheading'>7 places <MdArrowRight style={{ fontSize: '30px' }} /></p>
+            </div>
+          </div>
+
+        </div>
+      </section>
+
+
+
     </div>
   )
 }
