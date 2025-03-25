7a170615dc9e439e55e0e4d05c25bbc3b7bc80e6
lulullu7 <lulu.fathima.km@gmail.com>
2025-03-25 12:55:41 +0530

    fourth and fifth section zomato design completed

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -4,6 +4,8 @@ import { IoIosSearch } from "react-icons/io";
 import { FaLocationDot } from "react-icons/fa6";
 import { TiArrowSortedDown } from "react-icons/ti";
 import { MdArrowRight } from "react-icons/md";
+import { RiArrowRightSLine } from "react-icons/ri";
+import { FaChevronDown } from "react-icons/fa";
 
 function app() {
   return (
@@ -57,10 +59,10 @@ function app() {
             </div>
           </div>
           <div className='second_section_card_main'>
-            <img className='second_section_card_image' height={'170px'} width={'100%'} src="https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp" alt="" />
+            <img className='second_section_card_image' height={'170px'} width={'100%'} src="https://b.zmtcdn.com/webFrontend/d026b357feb0d63c997549f6398da8cc1647364915.jpeg?output-format=webp" alt="" />
             <div className='second_section_card_content'>
-              <p style={{ fontSize: '23px', color: 'black' }}>Order online</p>
-              <p style={{ marginTop: '5px' }}>stay home and order to your dooorstep</p>
+              <p style={{ fontSize: '23px', color: 'black' }}>Dining</p>
+              <p style={{ marginTop: '5px' }}>View the city's favourite dining venues</p>
             </div>
           </div>
 
@@ -89,21 +91,165 @@ function app() {
 
             </div>
             <div className='third_section_card_onebyone card_two'>
-              <p className='third_section_card_onebyone_heading'>Places for art lovers </p>
-              <p className='third_section_card_onebyone_subheading'>7 places <MdArrowRight style={{ fontSize: '30px' }} /></p>
+              <p className='third_section_card_onebyone_heading'>Great Cafes </p>
+              <p className='third_section_card_onebyone_subheading'>11 places <MdArrowRight style={{ fontSize: '30px' }} /></p>
             </div>
             <div className='third_section_card_onebyone card_three'>
-              <p className='third_section_card_onebyone_heading'>Places for art lovers</p>
-              <p className='third_section_card_onebyone_subheading'>7 places <MdArrowRight style={{ fontSize: '30px' }} /></p>
+              <p className='third_section_card_onebyone_heading'>Places for Authentic kuxhi..</p>
+              <p className='third_section_card_onebyone_subheading'>5 places <MdArrowRight style={{ fontSize: '30px' }} /></p>
             </div>
             <div className='third_section_card_onebyone card_four'>
-              <p className='third_section_card_onebyone_heading'>Places for art lovers </p>
-              <p className='third_section_card_onebyone_subheading'>7 places <MdArrowRight style={{ fontSize: '30px' }} /></p>
+              <p className='third_section_card_onebyone_heading'> Best Luxuary Dinig Places</p>
+              <p className='third_section_card_onebyone_subheading'>22 places <MdArrowRight style={{ fontSize: '30px' }} /></p>
             </div>
           </div>
 
         </div>
       </section>
+
+      {/* section 4 zomato */}
+
+      <section className='fourth_section_main'>
+        <div className='fourth_section_innerdiv_main'>
+          <div className='fourth_section_innerdiv_heading'>Popular localities in and around Kochi</div>
+
+          <div className='fourth_section_card_onebyone'>
+            <div className='fourth_section_card_onebyone_left'>
+              <p className='fourth_section_card_left_heading'>Kakanad</p>
+              <p className='fourth_section_card_left_subheading'>529 places</p>
+            </div>
+            <div className='fourth_section_card_onebyone_right'>
+              <RiArrowRightSLine style={{ fontSize: '20px', color: 'rgb(28, 27, 27)' }} />
+            </div>
+          </div>
+
+          <div className='fourth_section_card_onebyone'>
+            <div className='fourth_section_card_onebyone_left'>
+              <p className='fourth_section_card_left_heading'>Kakanad</p>
+              <p className='fourth_section_card_left_subheading'>529 places</p>
+            </div>
+            <div className='fourth_section_card_onebyone_right'>
+              <RiArrowRightSLine style={{ fontSize: '20px', color: 'rgb(28, 27, 27)' }} />
+            </div>
+          </div>
+
+          <div className='fourth_section_card_onebyone'>
+            <div className='fourth_section_card_onebyone_left'>
+              <p className='fourth_section_card_left_heading'>Kakanad</p>
+              <p className='fourth_section_card_left_subheading'>529 places</p>
+            </div>
+            <div className='fourth_section_card_onebyone_right'>
+              <RiArrowRightSLine style={{ fontSize: '20px', color: 'rgb(28, 27, 27)' }} />
+            </div>
+          </div>
+
+          <div className='fourth_section_card_onebyone'>
+            <div className='fourth_section_card_onebyone_left'>
+              <p className='fourth_section_card_left_heading'>Kakanad</p>
+              <p className='fourth_section_card_left_subheading'>529 places</p>
+            </div>
+            <div className='fourth_section_card_onebyone_right'>
+              <RiArrowRightSLine style={{ fontSize: '20px', color: 'rgb(28, 27, 27)' }} />
+            </div>
+          </div>
+
+          <div className='fourth_section_card_onebyone'>
+            <div className='fourth_section_card_onebyone_left'>
+              <p className='fourth_section_card_left_heading'>Kakanad</p>
+              <p className='fourth_section_card_left_subheading'>529 places</p>
+            </div>
+            <div className='fourth_section_card_onebyone_right'>
+              <RiArrowRightSLine style={{ fontSize: '20px', color: 'rgb(28, 27, 27)' }} />
+            </div>
+          </div>
+
+          <div className='fourth_section_card_onebyone'>
+            <div className='fourth_section_card_onebyone_left'>
+              <p className='fourth_section_card_left_heading'>Kakanad</p>
+              <p className='fourth_section_card_left_subheading'>529 places</p>
+            </div>
+            <div className='fourth_section_card_onebyone_right'>
+              <RiArrowRightSLine style={{ fontSize: '20px', color: 'rgb(28, 27, 27)' }} />
+            </div>
+          </div>
+
+          <div className='fourth_section_card_onebyone'>
+            <div className='fourth_section_card_onebyone_left'>
+              <p className='fourth_section_card_left_heading'>Kakanad</p>
+              <p className='fourth_section_card_left_subheading'>529 places</p>
+            </div>
+            <div className='fourth_section_card_onebyone_right'>
+              <RiArrowRightSLine style={{ fontSize: '20px', color: 'rgb(28, 27, 27)' }} />
+            </div>
+          </div>
+
+          <div className='fourth_section_card_onebyone'>
+            <div className='fourth_section_card_onebyone_left'>
+              <p className='fourth_section_card_left_heading'>Kakanad</p>
+              <p className='fourth_section_card_left_subheading'>529 places</p>
+            </div>
+            <div className='fourth_section_card_onebyone_right'>
+              <RiArrowRightSLine style={{ fontSize: '20px', color: 'rgb(28, 27, 27)' }} />
+            </div>
+          </div>
+
+          <div className='fourth_section_card_onebyone last_fourth'>
+            <p className='fourth_section_card_last'>see more  </p>
+            <FaChevronDown style={{ fontSize: '20px', color: 'rgb(28, 27, 27)' }} />
+          </div>
+        </div>
+      </section>
+
+      {/* section 5 zomato */}
+      <section className='fifth_section_main'>
+        <div className='fifth_section_innerdiv_main'>
+          <div className='fifth_section_innerdiv_left'>
+            <img
+              height={'450px'}
+              width={'450px'}
+              src="https://b.zmtcdn.com/data/o2_assets/ce5bc038a8a2d4f8f24465c8826182af1726501431.png" alt="poster" />
+          </div>
+          <div className='fifth_section_innerdiv_right'>
+            <h1 style={{ fontSize: '45px', marginBottom:'3%' }}>Get the Zomato app</h1>
+            <p className='fifth_section_innerdiv_right_subheading'>We will send you a link, open it on your phone to download the app</p>
+
+            <div className='fifth_section_innerdiv_right_radiomain'>
+              <div className='fifth_section_innerdiv_right_radio_input_onebyone'>
+                <input type="radio" name='contact' className='fifth_section_innerdiv_right_radio_input' />
+                Email
+              </div>
+              <div className='fifth_section_innerdiv_right_radio_input_onebyone'>
+                <input type="radio" name='contact' className='fifth_section_innerdiv_right_radio_inputtwo' />
+                Phone
+              </div>
+            </div>
+
+            <div className='fifth_section_innerdiv_right_input_main'>
+              <input type="text" placeholder='Email' className='fifth_section_innerdiv_right_input_email' />
+              <button className='fifth_section_innerdiv_right_input_sharebutton'>share app link</button>
+            </div>
+            <p className='fifth_section_innerdiv_right_download_app'>Download app from</p>
+            <div>
+              <img
+                height={'40px'}
+                width={'160px'}
+                style={{ marginRight: '10px' }}
+                src="https://b.zmtcdn.com/data/webuikit/23e930757c3df49840c482a8638bf5c31556001144.png"
+                alt="" />
+              <img
+                height={'40px'}
+                width={'160px'}
+                src="	https://b.zmtcdn.com/data/webuikit/9f0c85a5e33adb783fa0aef667075f9e1556003622.png"
+                alt="" />
+            </div>
+
+          </div>
+
+
+        </div>
+      </section>
+
+
 
 
 
